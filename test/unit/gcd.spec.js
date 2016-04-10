2d7f1e95796460736f24fd53f0a556c0fe9c0a04
Alexander Kasheverov <alexander.kasheverov@dataart.com>
2016-04-10 18:06:50 +0300

    formatting

--- test/unit/gcd.spec.js
+++ test/unit/gcd.spec.js
@@ -13,11 +13,8 @@ describe('Greatest common delimiter', function() {
         utils.gcd(1,1).should.equal(1, '1 and 1');
         utils.gcd(1,2).should.equal(1, '1 and 2');
         utils.gcd(5,10).should.equal(5, '5 and 10');
-    });
-
-    it('should calculate correctly', function () {
         expect(utils.gcd(undefined,2)).to.equal(null);
     });
-    
+
 });
 
